e1eb0e1824466e2a9d44f5fac7954e4abfc4da4e
Diyorbek Rustamjonov <diyorbek.ru@payme.uz>
2023-05-23 02:25:33 +0500

    [+] Created axios.config.ts register for application working with .env variables

---
+++ src/common/config/axios.config.ts
@@ -0,0 +1,6 @@
+import * as NestConfig from '@nestjs/config';
+
+export const AxiosConfig = NestConfig.registerAs('axios', () => ({
+    timeout: process.env.AXIOS_TIMEOUT || 2000,
+    maxRedirects: process.env.AXIOS_MAX_REDIRECTS || 3,
+}));
