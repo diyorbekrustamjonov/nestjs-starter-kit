66471059561f27a33182f1f2addccf6e931f094b
Diyorbek Rustamjonov <diyorbek.ru@payme.uz>
2023-05-23 02:35:01 +0500

    [+] Created mongodb.module.ts for connection database and DI for nest js

---
+++ src/core/shared/database/mongodb/mongodb.module.ts
@@ -0,0 +1,32 @@
+import * as NestCommon from '@nestjs/common';
+import * as NestConfig from '@nestjs/config';
+import * as NestMongoose from '@nestjs/mongoose';
+
+import * as Mongoose from 'mongoose';
+import * as MongooseSlugUpdater from 'mongoose-slug-updater';
+import * as Configs from 'src/common/config';
+
+
+@NestCommon.Module({
+    imports: [
+        NestMongoose.MongooseModule.forRootAsync({
+            imports: [NestConfig.ConfigModule],
+            inject: [NestConfig.ConfigService],
+            useFactory: async function (
+                configService: NestConfig.ConfigService,
+            ): Promise<NestMongoose.MongooseModuleOptions> {
+                const mongodb = configService.get<NestConfig.ConfigType<typeof Configs.MongodbConfig>>('mongodb');
+                return {
+                    uri: mongodb.uri,
+                    connectionFactory: async (connection: Mongoose.Connection): Promise<Mongoose.Connection> => {
+                        connection.plugin(MongooseSlugUpdater);
+                        return connection;
+                    },
+                };
+            },
+        }),
+    ],
+    providers: [],
+    exports: [NestMongoose.MongooseModule],
+})
+export class MongodbModule {}
