c3e1d6367a1ff341c7c7b16262dbd9826daffa67
Diyorbek Rustamjonov <diyorbek.ru@payme.uz>
2023-05-23 02:29:08 +0500

    [+] Created permissions.enum.ts in common/enums for application

---
+++ src/common/enums/permissions.enum.ts
@@ -0,0 +1,10 @@
+export enum RolePermission {
+    USER_CREATE = 'user:create',
+    USER_READ = 'user:read',
+    USER_UPDATE = 'user:update',
+    USER_DELETE = 'user:delete',
+    ROLE_CREATE = 'role:create',
+    ROLE_READ = 'role:read',
+    ROLE_UPDATE = 'role:update',
+    ROLE_DELETE = 'role:delete',
+}
