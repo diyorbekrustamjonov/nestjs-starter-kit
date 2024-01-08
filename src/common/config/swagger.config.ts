f3325aac3a093b2a9ff4ce654d22e380828a05d8
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-08 23:29:07 +0500

    change: removed server and sawagger configs

--- src/common/config/swagger.config.ts
+++
@@ -1,8 +0,0 @@
-import * as NestConfig from '@nestjs/config';
-
-export const SwaggerConfig = NestConfig.registerAs('swagger', () => ({
-    title: process.env.SWAGGER_TITLE || 'Nest Starter Kit',
-    description: process.env.SWAGGER_DESCRIPTION || 'The API documentation',
-    version: process.env.SWAGGER_VERSION || '1.2',
-    url: process.env.SWAGGER_URL || '/api-docs',
-}));
