19e2e6f3aaed7bbb7473c8d7d9956f944829c9e1
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-08 23:33:33 +0500

    style: changed styles prettier

--- src/core/shared/seeders/seeder.ts
+++ src/core/shared/seeders/seeder.ts
@@ -1,32 +1,30 @@
 import * as NestSeeder from 'nestjs-seeder';
 import * as NestMongoose from '@nestjs/mongoose';
 import * as NestConfig from '@nestjs/config';
-import * as Configs from 'common/config';
+import * as Configs from 'src/components/config';
 
 import * as Mocks from 'shared/seeders/mocks';
 import * as Schemas from 'core/schemas';
 
-NestSeeder
-    .seeder({
-        imports: [
-            NestConfig.ConfigModule.forRoot({
-                load: [Configs.MongodbConfig],
-                cache: true,
-            }),
-            NestMongoose.MongooseModule.forRootAsync({
-                imports: [NestConfig.ConfigModule],
-                useFactory: (configService: NestConfig.ConfigService) => ({
-                    uri: configService.get<string>('mongodb.uri'),
-                    useNewUrlParser: true,
-                }),
-                inject: [NestConfig.ConfigService],
-            }),
-            NestMongoose.MongooseModule.forFeature([
-                {
-                    name: Schemas.Role.name,
-                    schema: Schemas.RoleSchema,
-                },
-            ]),
-        ],
-    })
-    .run([Mocks.RolesSeeder]);
+NestSeeder.seeder({
+  imports: [
+    NestConfig.ConfigModule.forRoot({
+      load: [Configs.MongodbConfig],
+      cache: true,
+    }),
+    NestMongoose.MongooseModule.forRootAsync({
+      imports: [NestConfig.ConfigModule],
+      useFactory: (configService: NestConfig.ConfigService) => ({
+        uri: configService.get<string>('mongodb.uri'),
+        useNewUrlParser: true,
+      }),
+      inject: [NestConfig.ConfigService],
+    }),
+    NestMongoose.MongooseModule.forFeature([
+      {
+        name: Schemas.Role.name,
+        schema: Schemas.RoleSchema,
+      },
+    ]),
+  ],
+}).run([Mocks.RolesSeeder]);
